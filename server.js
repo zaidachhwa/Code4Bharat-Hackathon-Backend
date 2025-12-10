7265b9302d07edd6e475b634ab5183ad2408c4a5
zaidachhwa <zaidachhwa8486@gmail.com>
2025-12-10 18:13:51 +0530

    nfjgnre

--- server.js
+++ server.js
@@ -1,49 +1,45 @@
-import express from 'express';
-import connectDB from './db/db.js';
-import userRegister from './routes/userRegister.route.js';
-import ambassadorRegister from './routes/ambassadorRegister.route.js';
-import adminLogin from './routes/adminLogin.route.js';
-import cors from 'cors';
+import express from "express";
+import connectDB from "./db/db.js";
+import userRegister from "./routes/userRegister.route.js";
+import ambassadorRegister from "./routes/ambassadorRegister.route.js";
+import adminLogin from "./routes/adminLogin.route.js";
+import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import getAmbassadors from './routes/getAmbassadors.route.js';
-import ambassadorLogin from './routes/ambassadorLogin.route.js';
-import ambassadorStep1FormData from './routes/ambassadorStep1FormData.route.js';
-import ambassadorStep2FormData from './routes/ambassadorStep2FormData.route.js';
-import ambassadorStep3 from './routes/ambassadorStep3.route.js';
-import ambassadorDashboard from './routes/ambassadorDashboard.route.js';
-import ambassadorCouponCodeUsers from './routes/ambassadorCouponCodeUsers.route.js';
-import imagesOfPromotionAndSeminar from './routes/imagesOfPromotionAndSeminar.route.js';
-import registration from './routes/registration.route.js';
-
-
+import getAmbassadors from "./routes/getAmbassadors.route.js";
+import ambassadorLogin from "./routes/ambassadorLogin.route.js";
+import ambassadorStep1FormData from "./routes/ambassadorStep1FormData.route.js";
+import ambassadorStep2FormData from "./routes/ambassadorStep2FormData.route.js";
+import ambassadorStep3 from "./routes/ambassadorStep3.route.js";
+import ambassadorDashboard from "./routes/ambassadorDashboard.route.js";
+import ambassadorCouponCodeUsers from "./routes/ambassadorCouponCodeUsers.route.js";
+import imagesOfPromotionAndSeminar from "./routes/imagesOfPromotionAndSeminar.route.js";
+import registration from "./routes/registration.route.js";
 
 const app = express();
 dotenv.config();
 connectDB();
 
-
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.DOMAIN,
-    credentials: true
-}));
-app.use(express.urlencoded({ extended: false }))
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
 app.use("/uploads", express.static("uploads"));
 
-
-
 app.use("/api/users", userRegister);
-app.use("/api/admin", adminLogin)
+app.use("/api/admin", adminLogin);
 
 app.use(getAmbassadors);
-app.use(ambassadorRegister)
+app.use(ambassadorRegister);
 app.use(ambassadorLogin);
 
 app.use(ambassadorStep3);
 app.use(ambassadorDashboard);
-
 
 app.use(ambassadorStep1FormData);
 app.use(ambassadorStep2FormData);
@@ -52,14 +48,11 @@ app.use(imagesOfPromotionAndSeminar);
 
 app.use(registration);
 
-
 app.get("/", (req, res) => {
-    res.send("Backend is running☑️");
-})
+  res.send("Backend is running☑️");
+});
 
-
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
-    console.log(`Server is running at PORT: http://localhost:${PORT}`);
-})
+  console.log(`Server is running at PORT: http://localhost:${PORT}`);
+});
